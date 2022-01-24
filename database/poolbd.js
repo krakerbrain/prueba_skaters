e8644f1144dc03347b7b6c46f05e979f0f6d3418
krakerbrain <marioplantabaja@gmail.com>
2022-01-24 19:27:30 -0300

    Preparando despliegue

--- database/poolbd.js
+++ database/poolbd.js
@@ -1,6 +1,20 @@
 const { Pool } = require("pg");
 
 const config = {
+  user: "mgamgxookbxcxb",
+  host: "ec2-34-233-214-228.compute-1.amazonaws.com",
+  database: "d1h33nodfdmc20",
+  password: "3c194935ceb2334b09746805045539c805505fe656e5f5ad1f6b22fa4be7a7dc",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  max: 20,
+  idleTimeoutMillis: 5000,
+  connectionTimeoutMillis: 2000,
+};
+
+/* const config = {
   user: "mario",
   host: "localhost",
   database: "skatepark",
@@ -10,7 +24,7 @@ const config = {
   idleTimeoutMillis: 5000,
   connectionTimeoutMillis: 2000,
 };
-
+ */
 const Singleton = (function () {
   var instance;
 
